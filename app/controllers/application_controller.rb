f75b88a5cfc811c3316cf350283dc7bdd67fa9f5
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-07 10:58:29 -0400

    Hard-code user log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  helpers do
+    def current_user
+      # mocked version of being loggd in
+      User.first
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 end
