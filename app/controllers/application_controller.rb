e87814bb5c899b5a112717747c24b3e6193073c7
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-07 11:24:51 -0400

    Remove helpers do before helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::API
-  helpers do
-    def current_user
-      # mocked version of being loggd in
-      User.first
-    end
+  # does this need to be private?
+  def current_user
+    # mocked version of being loggd in
+    User.first
+  end
 
-    def logged_in?
-      !!current_user
-    end
+  def logged_in?
+    !!current_user
   end
 end
