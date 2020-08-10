33776027da8e65a734b86646c4157e19880d383b
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-10 18:26:10 -0400

    Add get route for get_current_user and post route for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "/api/v1/login", to: "sessions#create"
-  
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+
+
   namespace :api do
     namespace :v1 do
       resources :users
