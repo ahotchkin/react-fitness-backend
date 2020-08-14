536d4ffdcf1da25cd8e65bac964924859ad98f6b
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-14 18:13:13 -0400

    Add create method and private methods for set_user and user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  # before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy]
 
   def index
     @users = User.all
@@ -14,4 +14,27 @@ class Api::V1::UsersController < ApplicationController
     # render json: @user, status: 200
     render json: user_json
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
+    else
+      resp = {
+        :error => @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+    def set_user
+      @user = User.find(params.id)
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :password, :gender, :age, :height_feet, :height_inches, :weight)
+    end
 end
