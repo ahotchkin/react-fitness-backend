1841fa513416caa44b4b196450d48c7c28dd5d9c
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-14 11:58:49 -0400

    Use ExerciseSerializer to serialize exercise data

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::ExercisesController < ApplicationController
     if logged_in?
       @exercises = current_user.exercises
 
-      render json: @exercises, status: 200
+      render json: ExerciseSerializer.new(@exercises), status: 200
     else
       render json: {
         error: "You must be logged in to see exercises"
