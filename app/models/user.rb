8ec11d0438e8214018b647f221a6a7bcfb3ebae0
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-14 18:13:31 -0400

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :exercises
   has_many :diaries
-  has_secure_password
+
+  validates :username, :gender, :age, :height_feet, :height_inches, :weight, presence: true
+  validates :username, uniqueness: true
 end
+
+  # t.integer "daily_calorie_goal"
