b1285298aff1d8f9221c773edd01bb7eea33daf9
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-14 12:00:39 -0400

    Add attribute of exercises and manually list out which data will come through for exercises

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,19 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username, :gender, :age, :height_feet, :height_inches, :weight, :daily_calorie_goal
+
+  # has_many :exercises, serializer: ExerciseSerializer
+
+  # will list exercises as an attribute instead of a relationship, breaking json convention. To list as a relationship add :exercises to attributes above. Is there a way to control info that shows by doing it that awy?
+  attribute :exercises do |user|
+    user.exercises.map do |exercise|
+      {
+        :category => exercise.category,
+        :name => exercise.name,
+        :duration_in_minutes => exercise.duration_in_minutes,
+        :calories_burned => exercise.calories_burned
+      }
+    end
+  end
+
 end
