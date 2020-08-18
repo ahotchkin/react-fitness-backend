f7f5f1da56952d18f3283f68a121b781ea84dd99
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-18 12:43:40 -0400

    Add exercise validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,8 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+
+  # do I really need anything except calories_burned???
+  validates :user, :category, :name, :duration_in_minutes, :calories_burned, presence: true
 end
+
+# add validation for exercise category? or not needed because form is drop down?
