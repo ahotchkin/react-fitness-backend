132ab6ec31436f3f8718053062b9188893b152f8
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-19 21:23:30 -0400

    Update set_user method and use in show method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     user_json = UserSerializer.new(@user).serialized_json
 
     # render json: @user, status: 200
@@ -31,7 +31,7 @@ class Api::V1::UsersController < ApplicationController
 
   private
     def set_user
-      @user = User.find(params.id)
+      @user = User.find(params[:id])
     end
 
     def user_params
