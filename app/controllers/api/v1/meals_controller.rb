972a1149658d4f37a943c67b76ab8e93cb975d1b
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-20 17:21:29 -0400

    Add index action to display current_user's meals

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::MealsController < ApplicationController
+
+  def index
+
+    if logged_in?
+      # binding.pry
+      @meals = current_user.diaries.map do |diary|
+        diary.meals
+      end
+      render json: @meals, status: 200
+      # ************* NEED TO GET THE SERIALIZER TO WORK. CURRENT ERROR: id is a mandatory field in the jsonapi spec
+      # render json: MealSerializer.new(@meals), status: 200
+    else
+      render json: {
+        error: "You must be logged in to see meals"
+      }
+    end
+  end
+
 end
