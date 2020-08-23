c4770238c239c480a1ad3da876d8f532a8421c12
ahotchkin <allyson.hotchkin@gmail.com>
2020-08-23 18:04:32 -0400

    Add index method to render all meal_foods from the database as json

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::MealFoodsController < ApplicationController
+
+  def index
+    @meal_food = MealFood.all
+
+    render json: MealFoodSerializer.new(@meal_food), status: 200
+  end
 end
