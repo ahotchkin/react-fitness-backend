552e6792d61be42147b1915c9f41216ff2dd936a
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-11 15:09:20 -0400

    Add create method and private diary_params method

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -23,27 +23,27 @@ class Api::V1::DiariesController < ApplicationController
   #   end
   # end
   #
-  # def create
-  #   # should I use build to build on the user's exercises? if I do that do I need to pass in the user_id?????
-  #   @exercise = Exercise.new(exercise_params)
-  #   if @exercise.save
-  #     render json: ExerciseSerializer.new(@exercise), status: :created
-  #   else
-  #     resp = {
-  #       :error => @exercise.errors.full_messages.to_sentence
-  #     }
-  #     render json: resp, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+    # should I use build to build on the user's diaries? if I do that do I need to pass in the user_id?????
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      render json: DiarySerializer.new(@diary), status: :created
+    else
+      resp = {
+        :error => @diary.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
   #
   #
-  # private
-  #   # def set_user
-  #   #   @user = User.find(params.id)
-  #   # end
-  #
-  #   def exercise_params
-  #     params.require(:exercise).permit(:user_id, :category, :name, :duration_in_minutes, :calories_burned)
-  #   end
+  private
+    # def set_user
+    #   @user = User.find(params.id)
+    # end
+
+    def diary_params
+      params.require(:diary).permit(:user_id, :date)
+    end
 
 end
