ae1472accffd3b55a201d29eda8d859a25e50a6e
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-14 21:36:35 -0400

    Add create method and private method for meal_food_params

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -5,4 +5,24 @@ class Api::V1::MealFoodsController < ApplicationController
 
     render json: MealFoodSerializer.new(@meal_food), status: 200
   end
+
+  def create
+    # should I use build to build on the user's exercises? if I do that do I need to pass in the user_id?????
+    @meal_food = MealFood.new(meal_food_params)
+    if @meal_food.save
+      render json: MealFoodSerializer.new(@meal_food), status: :created
+    else
+      resp = {
+        :error => @meal_food.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def meal_food_params
+      params.require(:meal_food).permit(:meal_id, :food_id, :number_of_servings, :calories)
+    end
+
 end
