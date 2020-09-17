e34d54936db6af9edd8f38b76c0abb69f68ae037
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-17 17:48:16 -0400

    Add update method and private methods for current_meal and meal_params

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -18,4 +18,27 @@ class Api::V1::MealsController < ApplicationController
     end
   end
 
+  def update
+    if current_meal.update(meal_params)
+      render json: MealSerializer.new(current_meal), status: 200
+    else
+      resp = {
+        :error => @meal.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+    def current_meal
+      meal = Meal.find_by_id(params[:id])
+    end
+
+    def meal_params
+      params.require(:meal).permit(:diary_id, :category, :calories)
+    end
+
+
 end
