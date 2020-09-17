ab09a5904aa88aa313d382f193285b17930385e8
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-17 15:31:32 -0400

    Add comment to remove meal as attribute if no longer needed

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,5 +1,6 @@
 class MealSerializer
   include FastJsonapi::ObjectSerializer
+  # remove meal_foods if I end up restructuring??????
   attributes :category, :calories, :meal_foods, :foods
 
   belongs_to :diary
