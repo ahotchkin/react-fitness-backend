e92740b900d00be5506d6887445c3743a250439e
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-16 20:39:21 -0400

    Add update method and private method for current_meal_food

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::MealFoodsController < ApplicationController
   end
 
   def create
-    # should I use build to build on the user's exercises? if I do that do I need to pass in the user_id?????
+    # should I use build to build on the user's meal_foods? if I do that do I need to pass in the user_id?????
     @meal_food = MealFood.new(meal_food_params)
     if @meal_food.save
       render json: MealFoodSerializer.new(@meal_food), status: :created
@@ -19,7 +19,22 @@ class Api::V1::MealFoodsController < ApplicationController
     end
   end
 
+  def update
+    if current_meal_food.update(meal_food_params)
+      render json: MealFoodSerializer.new(current_meal_food), status: 200
+    else
+      resp = {
+        :error => @meal_food.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
   private
+
+    def current_meal_food
+      meal_food = Exercise.find_by_id(params[:id])
+    end
 
     def meal_food_params
       params.require(:meal_food).permit(:meal_id, :food_id, :number_of_servings, :calories)
