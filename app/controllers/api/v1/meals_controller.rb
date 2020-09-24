98355eff6aec2742e22cfd1ef23f53f316c31abf
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-24 14:12:33 -0400

    Update index method to create array of all meals to pass as parameter to MealSerializer

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -1,16 +1,19 @@
 class Api::V1::MealsController < ApplicationController
 
   def index
-
     if logged_in?
-      # can only show meals for one diary at a time, which is okay for what I want. But is there a way to show ALL meals at once while using Serializer? If I just render json: @meals, I get an error on the frontend
+      # # can only show meals for one diary at a time, which is okay for what I want. But is there a way to show ALL meals at once while using Serializer? If I just render json: @meals, I get an error on the frontend
+      #
+      # # gives an error if there is no current diary. maybe don't show this on signup????? or automatically create diary on signup?????
+      # @current_diary = current_user.diaries.find_by :date => Date.today
+      # @current_meals = @current_diary.meals
+      # # render json: @meals, status: 200
+      # # ************* NEED TO GET THE SERIALIZER TO WORK. CURRENT ERROR: id is a mandatory field in the jsonapi spec
+      # render json: MealSerializer.new(@current_meals), status: 200
+      @meals = []
+      current_user.diaries.each { |diary| @meals << diary.meals }
+      render json: MealSerializer.new(@meals.flatten), status: 200
 
-      # gives an error if there is no current diary. maybe don't show this on signup????? or automatically create diary on signup?????
-      @current_diary = current_user.diaries.find_by :date => Date.today
-      @current_meals = @current_diary.meals
-      # render json: @meals, status: 200
-      # ************* NEED TO GET THE SERIALIZER TO WORK. CURRENT ERROR: id is a mandatory field in the jsonapi spec
-      render json: MealSerializer.new(@current_meals), status: 200
     else
       render json: {
         error: "You must be logged in to see meals"
