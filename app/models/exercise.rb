8b1239a0a47e596240314f5cd9dd7b47b16474a5
ahotchkin <allyson.hotchkin@gmail.com>
2020-09-30 15:26:05 -0400

    Add date to validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,7 @@ class Exercise < ApplicationRecord
   belongs_to :user
 
   # do I really need anything except calories_burned???
-  validates :user, :category, :name, :duration_in_minutes, :calories_burned, presence: true
+  validates :user, :date, :category, :name, :duration_in_minutes, :calories_burned, presence: true
 end
 
 # add validation for exercise category? or not needed because form is drop down?
