4d4b548f9ad5266138441ce989dc7421b282a496
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-06 15:05:46 -0400

    Add validations for required information

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
   has_many :meal_foods
   has_many :meals, through: :meal_foods
+
+  validates :brand_name, :description, :serving_size, :servings_per_container, :calories, :total_fat, :total_carbohydrate, :protein, presence: true
+
+  # validate where input should be integer
 end
