5c724d50b9725ceb0b9119d712d21390c1538fc8
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-08 16:19:00 -0400

    Add additional nutrients as attributes and add comment reminder to revisit and possibly change

--- app/serializers/meal_food_serializer.rb
+++ app/serializers/meal_food_serializer.rb
@@ -1,8 +1,11 @@
 class MealFoodSerializer
   include FastJsonapi::ObjectSerializer
   # keep meal and food if I end up restructuring?????
-  attributes :number_of_servings, :calories, :meal, :food
+  attributes :number_of_servings, :calories, :meal, :food, :calories, :total_fat, :saturated_fat, :polyunsaturated_fat, :monounsaturated_fat, :trans_fat, :cholesterol, :sodium, :total_carbohydrate, :dietary_fiber, :total_sugars, :added_sugars, :sugar_alcohols, :protein, :vitamin_a, :vitamin_c, :vitamin_d, :calcium, :iron, :potassium
 
   belongs_to :meal
   belongs_to :food
+
+
+  # Added all nutrient attributes to have access, could also do this on frontend since food is an attribute of mealFood. Would need to multiply food.attributes.nutrient * number_of_servings to get information. What is the best way to do this?????????
 end
