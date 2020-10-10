407689ba707b9f318eb6ad6228c5d32e5194490c
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-10 14:44:42 -0400

    Comment out exercises and diaries attributes as they are not needed

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,28 +4,28 @@ class UserSerializer
 
   # has_many :exercises, serializer: ExerciseSerializer
 
-  # will list exercises as an attribute instead of a relationship, breaking json convention. To list as a relationship add :exercises to attributes above. Is there a way to control info that shows by doing it that awy?
-  attribute :exercises do |user|
-    user.exercises.map do |exercise|
-      {
-        :date => exercise.date,
-        :category => exercise.category,
-        :name => exercise.name,
-        :duration_in_minutes => exercise.duration_in_minutes,
-        :calories_burned => exercise.calories_burned
-      }
-    end
-  end
+  # will list exercises as an attribute instead of a relationship, breaking json convention. To list as a relationship add :exercises to attributes above. Is there a way to control info that shows by doing it that way?
+  # attribute :exercises do |user|
+  #   user.exercises.map do |exercise|
+  #     {
+  #       :date => exercise.date,
+  #       :category => exercise.category,
+  #       :name => exercise.name,
+  #       :duration_in_minutes => exercise.duration_in_minutes,
+  #       :calories_burned => exercise.calories_burned
+  #     }
+  #   end
+  # end
 
-  attribute :diaries do |user|
-    user.diaries.map do |diary|
-      {
-        :date => diary.date,
-        :meals => diary.meals,
-        # would also like food to be nested under meals. how do I do that????????????
-      }
-    end
-  end
+  # attribute :diaries do |user|
+  #   user.diaries.map do |diary|
+  #     {
+  #       :date => diary.date,
+  #       :meals => diary.meals,
+  #       # would also like food to be nested under meals. how do I do that????????????
+  #     }
+  #   end
+  # end
 
   # attribute :meals do |user|
   #   user.diaries.map do |diary|
