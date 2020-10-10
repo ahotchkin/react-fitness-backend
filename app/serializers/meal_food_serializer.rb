b88bfdb05324f83220cfdc09c2b8708e3283fbe1
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-10 14:43:21 -0400

    Remove meal attribute as it is not needed

--- app/serializers/meal_food_serializer.rb
+++ app/serializers/meal_food_serializer.rb
@@ -1,7 +1,10 @@
 class MealFoodSerializer
   include FastJsonapi::ObjectSerializer
   # keep meal and food if I end up restructuring?????
-  attributes :number_of_servings, :calories, :meal, :food, :calories, :total_fat, :saturated_fat, :polyunsaturated_fat, :monounsaturated_fat, :trans_fat, :cholesterol, :sodium, :total_carbohydrate, :dietary_fiber, :total_sugars, :added_sugars, :sugar_alcohols, :protein, :vitamin_a, :vitamin_c, :vitamin_d, :calcium, :iron, :potassium
+  # attributes :number_of_servings, :calories, :meal, :food, :calories, :total_fat, :saturated_fat, :polyunsaturated_fat, :monounsaturated_fat, :trans_fat, :cholesterol, :sodium, :total_carbohydrate, :dietary_fiber, :total_sugars, :added_sugars, :sugar_alcohols, :protein, :vitamin_a, :vitamin_c, :vitamin_d, :calcium, :iron, :potassium
+
+  attributes :number_of_servings, :calories, :food, :calories, :total_fat, :saturated_fat, :polyunsaturated_fat, :monounsaturated_fat, :trans_fat, :cholesterol, :sodium, :total_carbohydrate, :dietary_fiber, :total_sugars, :added_sugars, :sugar_alcohols, :protein, :vitamin_a, :vitamin_c, :vitamin_d, :calcium, :iron, :potassium
+
 
   belongs_to :meal
   belongs_to :food
