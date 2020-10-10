aaabc5081f1c97954bad500e3689b233187dfb09
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-10 14:42:52 -0400

    Comment out meals attribute as it is not needed

--- app/serializers/diary_serializer.rb
+++ app/serializers/diary_serializer.rb
@@ -4,13 +4,13 @@ class DiarySerializer
 
   belongs_to :user
 
-  attribute :meals do |diary|
-    diary.meals.map do |meal|
-      {
-        :category => meal.category,
-        :calories => meal.calories,
-        :foods => meal.foods
-      }
-    end
-  end
+  # attribute :meals do |diary|
+  #   diary.meals.map do |meal|
+  #     {
+  #       :category => meal.category,
+  #       :calories => meal.calories,
+  #       :foods => meal.foods
+  #     }
+  #   end
+  # end
 end
