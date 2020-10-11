b8ede5fed1b06248d6e6da0a0a6cdf83eb243c18
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-11 15:34:16 -0400

    Add default value for nutrients user is not required to enter and reset db

--- db/migrate/20200805013546_create_foods.rb
+++ db/migrate/20200805013546_create_foods.rb
@@ -7,24 +7,24 @@ class CreateFoods < ActiveRecord::Migration[6.0]
       t.integer :servings_per_container
       t.integer :calories
       t.integer :total_fat
-      t.integer :saturated_fat
-      t.integer :polyunsaturated_fat
-      t.integer :monounsaturated_fat
-      t.integer :trans_fat
-      t.integer :cholesterol
-      t.integer :sodium
+      t.integer :saturated_fat, default: 0
+      t.integer :polyunsaturated_fat, default: 0
+      t.integer :monounsaturated_fat, default: 0
+      t.integer :trans_fat, default: 0
+      t.integer :cholesterol, default: 0
+      t.integer :sodium, default: 0
       t.integer :total_carbohydrate
-      t.integer :dietary_fiber
-      t.integer :total_sugars
-      t.integer :added_sugars
-      t.integer :sugar_alcohols
+      t.integer :dietary_fiber, default: 0
+      t.integer :total_sugars, default: 0
+      t.integer :added_sugars, default: 0
+      t.integer :sugar_alcohols, default: 0
       t.integer :protein
-      t.integer :vitamin_a
-      t.integer :vitamin_c
-      t.integer :vitamin_d
-      t.integer :calcium
-      t.integer :iron
-      t.integer :potassium
+      t.integer :vitamin_a, default: 0
+      t.integer :vitamin_c, default: 0
+      t.integer :vitamin_d, default: 0
+      t.integer :calcium, default: 0
+      t.integer :iron, default: 0
+      t.integer :potassium, default: 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -39,24 +39,24 @@ ActiveRecord::Schema.define(version: 2020_08_05_015444) do
     t.integer "servings_per_container"
     t.integer "calories"
     t.integer "total_fat"
-    t.integer "saturated_fat"
-    t.integer "polyunsaturated_fat"
-    t.integer "monounsaturated_fat"
-    t.integer "trans_fat"
-    t.integer "cholesterol"
-    t.integer "sodium"
+    t.integer "saturated_fat", default: 0
+    t.integer "polyunsaturated_fat", default: 0
+    t.integer "monounsaturated_fat", default: 0
+    t.integer "trans_fat", default: 0
+    t.integer "cholesterol", default: 0
+    t.integer "sodium", default: 0
     t.integer "total_carbohydrate"
-    t.integer "dietary_fiber"
-    t.integer "total_sugars"
-    t.integer "added_sugars"
-    t.integer "sugar_alcohols"
+    t.integer "dietary_fiber", default: 0
+    t.integer "total_sugars", default: 0
+    t.integer "added_sugars", default: 0
+    t.integer "sugar_alcohols", default: 0
     t.integer "protein"
-    t.integer "vitamin_a"
-    t.integer "vitamin_c"
-    t.integer "vitamin_d"
-    t.integer "calcium"
-    t.integer "iron"
-    t.integer "potassium"
+    t.integer "vitamin_a", default: 0
+    t.integer "vitamin_c", default: 0
+    t.integer "vitamin_d", default: 0
+    t.integer "calcium", default: 0
+    t.integer "iron", default: 0
+    t.integer "potassium", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
