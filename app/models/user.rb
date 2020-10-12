a662980664423c5359135793fce30636758d3a0c
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-12 12:13:19 -0400

    Update necessary validations to only_float from only_integer

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,9 @@ class User < ApplicationRecord
   validates :username, :gender, :lifestyle, presence: true
   validates :username, uniqueness: true
   validates :age, :numericality => { :only_integer => true, :greater_than_or_equal_to => 18, message: "must be 18 or older to sign up" }
-  # validates :height_feet, :height_inches, :weight, :daily_calorie_goal, :numericality => { :only_integer => true, :greater_than => 0 }
-
-  validates :height_feet, :weight, :daily_calorie_goal, :numericality => { :only_integer => true, :greater_than => 0 }
-
-  validates :daily_calorie_goal, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1200 }
+  validates :height_feet, :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :height_inches, :numericality => { :only_float => true }
+  validates :weight, :numericality => { :only_float => true, :greater_than => 0 }
 end
 
   # add validation for gender? or not needed because form is drop down?
