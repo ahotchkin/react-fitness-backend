7f039a45f58e4d36a50a02cba395b0238800e725
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-12 12:12:58 -0400

    Update necessary validations to only_float from only_integer

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,6 +3,7 @@ class Food < ApplicationRecord
   has_many :meals, through: :meal_foods
 
   validates :brand_name, :description, :serving_size, presence: true
-  validates :servings_per_container, :calories, :numericality => { :only_integer => true, :greater_than => 0 }
-  validates :total_fat, :total_carbohydrate, :protein, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+  validates :servings_per_container, :numericality => { :only_float => true, :greater_than => 0 }
+  validates :calories, :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :total_fat, :total_carbohydrate, :protein, :numericality => { :only_float => true, :greater_than_or_equal_to => 0 }
 end
