77fdda3b28983f4915a5d49da74f88840f63f371
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-25 19:34:24 -0400

    Add all user attributes to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :username, :gender, :age, :height_feet, :height_inches, :weight, :daily_calorie_goal
+  attributes :username, :gender, :age, :height_feet, :height_inches, :weight, :daily_calorie_goal, :daily_fat_goal, :daily_saturated_fat_goal, :daily_polyunsaturated_fat_goal, :daily_monounsaturated_fat_goal, :daily_trans_fat_goal, :daily_cholesterol_goal, :daily_sodium_goal, :daily_carbohydrate_goal, :daily_fiber_goal, :daily_sugar_goal, :daily_protein_goal, :daily_vitamin_a_goal, :daily_vitamin_c_goal, :daily_vitamin_d_goal, :daily_calcium_goal, :daily_iron_goal, :daily_potassium_goal
 
   # has_many :exercises, serializer: ExerciseSerializer
 
