e8e00907b0a33794187a1138881295f434f46555
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-25 13:52:05 -0400

    Add nutrient goal fields to user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -35,6 +35,6 @@ class Api::V1::UsersController < ApplicationController
     # end
 
     def user_params
-      params.require(:user).permit(:username, :password, :gender, :age, :height_feet, :height_inches, :weight, :lifestyle, :daily_calorie_goal)
+      params.require(:user).permit(:username, :password, :gender, :age, :height_feet, :height_inches, :weight, :lifestyle, :daily_calorie_goal, :daily_fat_goal, :daily_saturated_fat_goal, :daily_polyunsaturated_fat_goal, :daily_monounsaturated_fat_goal, :daily_carbohydrate_goal, :daily_sugar_goal, :daily_protein_goal, :daily_vitamin_a_goal, :daily_vitamin_c_fat_goal, :daily_vitamin_d_fat_goal, :daily_calcium_goal, :daily_iron_goal)
     end
 end
