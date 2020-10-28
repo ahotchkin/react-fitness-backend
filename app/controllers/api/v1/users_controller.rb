1052a66e0b7134b2c989f0344ee2d8c606d1a7cd
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-28 18:23:40 -0400

    Remove params that are assigned a default value in database

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -35,6 +35,6 @@ class Api::V1::UsersController < ApplicationController
     # end
 
     def user_params
-      params.require(:user).permit(:username, :password, :gender, :age, :height_feet, :height_inches, :weight, :lifestyle, :daily_calorie_goal, :daily_fat_goal, :daily_saturated_fat_goal, :daily_polyunsaturated_fat_goal, :daily_monounsaturated_fat_goal, :daily_trans_fat_goal, :daily_cholesterol_goal, :daily_sodium_goal, :daily_carbohydrate_goal, :daily_fiber_goal, :daily_sugar_goal, :daily_protein_goal, :daily_vitamin_a_goal, :daily_vitamin_c_goal, :daily_vitamin_d_goal, :daily_calcium_goal, :daily_iron_goal, :daily_potassium_goal)
+      params.require(:user).permit(:username, :password, :gender, :age, :height_feet, :height_inches, :weight, :lifestyle, :daily_calorie_goal, :daily_fat_goal, :daily_saturated_fat_goal, :daily_polyunsaturated_fat_goal, :daily_monounsaturated_fat_goal, :daily_carbohydrate_goal, :daily_sugar_goal, :daily_protein_goal, :daily_vitamin_a_goal, :daily_vitamin_c_goal, :daily_vitamin_d_goal, :daily_calcium_goal, :daily_iron_goal)
     end
 end
