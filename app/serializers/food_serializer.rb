65ee451ce90b4f20f416dfd521d3d665b7eb2f63
ahotchkin <allyson.hotchkin@gmail.com>
2020-10-30 13:51:45 -0400

    Add relationship belongs_to user

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -2,4 +2,5 @@ class FoodSerializer
   include FastJsonapi::ObjectSerializer
   attributes :brand_name, :description, :serving_size, :servings_per_container, :calories, :total_fat, :saturated_fat, :polyunsaturated_fat, :monounsaturated_fat, :trans_fat, :cholesterol, :sodium, :total_carbohydrate, :dietary_fiber, :total_sugars, :added_sugars, :sugar_alcohols, :protein, :vitamin_a, :vitamin_c, :vitamin_d, :calcium, :iron, :potassium
 
+  belongs_to :user
 end
