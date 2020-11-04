2951a312823baf533528437e07b496417ef56c5b
ahotchkin <allyson.hotchkin@gmail.com>
2020-11-04 15:36:25 -0500

    Remove routes that aren't being used

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,15 @@ Rails.application.routes.draw do
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
 
-
   namespace :api do
     namespace :v1 do
-      resources :users
-      resources :exercises
-      resources :diaries
-      resources :meals do
-        resources :foods
+      resources :exercises, except: :show
+      resources :diaries, only: [:index, :create]
+      resources :meals, only: [:index, :update] do
+        resources :foods, only: [:index]
       end
-      resources :foods
-      resources :meal_foods
+      resources :foods, only: [:index, :create]
+      resources :meal_foods, except: :show
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
